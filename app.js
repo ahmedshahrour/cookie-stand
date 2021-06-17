c6333db8f41864b559425553cc23ead43efea9ca
ahmedshahrour <ahmed89sh@gmail.com>
2021-06-17 21:25:55 +0300

    change code

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+  
 'use strict';
 
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
@@ -147,4 +148,3 @@ function formSubmission(event) {
 }
 
   salesdata.addEventListener('submit', formSubmission);
-  
