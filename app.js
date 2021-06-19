fe8754af99f26d3da3e9d5b0fc6c3a8a26c43303
ahmedshahrour <ahmed89sh@gmail.com>
2021-06-19 17:14:05 +0300

    newstyle

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-let salesdata = document.getElementById('salesdata');
+let salesdata = document.getElementById('sales');
 
 function SalmonCookies(name, min, max, average) {
     this.name = name;
